0f920fc2dd969a53e8382fa7389ad80c82f2b49c
mtizhoush <39172640+mtizhoush@users.noreply.github.com>
2019-03-18 03:11:55 -0700

    Add files via upload

---
+++ Whats that AnimalTests/Whats_that_AnimalTests.swift
@@ -0,0 +1,34 @@
+//
+//  Whats_that_AnimalTests.swift
+//  Whats that AnimalTests
+//
+//  Created by Mahsha Tizhoush on 3/4/19.
+//  Copyright © 2019 Mahsha. All rights reserved.
+//
+
+import XCTest
+@testable import Whats_that_Animal
+
+class Whats_that_AnimalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
